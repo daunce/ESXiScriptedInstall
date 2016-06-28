bfe68e4386e253b4cbcea1de36d6fb46721be312
daunce <andrew@dauncey.com.au>
2016-06-28 13:45:37 +1000

    mid day commit

---
+++ FromCDROM/build/networking.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+import os, commands, csv, subprocess
+
+MAC=subprocess.check_output("esxcli network ip interface list |grep MAC",shell=True)
+
+MACADDR = MAC.split()
+
+print "MAC = " + MACADDR[2]
+
+with open('/vmfs/volumes/datastore1/ListOfHosts.csv', 'rb') as f:
+        reader = csv.reader(f)
+        for row in reader:
+                print "Line " + row[0] + " " + row[1]
+                print "Comparing '%r' and '%r' " % (MACADDR[2], row[1])
+                if MACADDR[2] == row[1]:
+                        print "MATCHED + row[1]"
+                        os.system("esxcli system hostname set --fqdn=" + row[0])
+                        os.system("esxcli network ip interface ipv4 set --interface-name=vmk0 --ipv4=" + row[2] + " --netmask=" + row[3] + " --type=static")
+print "End of loop"
+print "Hostname is: "
+os.system("esxcli system hostname get")
+
+print MACADDR
+print MACTEST
+print "'%r'" % (MACTEST)
